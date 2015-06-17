c34fc3e46f0ba868eba369dc884fc92af753218a
Serge Tyatin <700@2rba.com>
2015-06-17 13:50:35 +0300

    deploy

--- config/puma.rb
+++ config/puma.rb
@@ -23,7 +23,7 @@ state_path "#{app_dir}/tmp/puma/state"
 activate_control_app "unix://#{app_dir}/tmp/puma/ctl.sock"
 
 on_worker_boot do
-  # require "active_record"
-  # ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-  # ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+  require "active_record"
+  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
 end
