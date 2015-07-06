5d2c1aeede7a8d05cacc79c569eafb91e8ea4dde
Serge Tyatin <700@2rba.com>
2015-07-06 23:32:04 +0300

    Cleans controllers from extra scaffold methods

--- app/controllers/car_requests_controller.rb
+++ app/controllers/car_requests_controller.rb
@@ -8,20 +8,6 @@ class CarRequestsController < ApplicationController
     @car_requests = CarRequest.all
   end
 
-  # GET /car_requests/1
-  # GET /car_requests/1.json
-  def show
-  end
-
-  # GET /car_requests/new
-  def new
-    @car_request = CarRequest.new
-  end
-
-  # GET /car_requests/1/edit
-  def edit
-  end
-
   # POST /car_requests
   # POST /car_requests.json
   def create
@@ -52,16 +38,6 @@ class CarRequestsController < ApplicationController
     end
   end
 
-  # DELETE /car_requests/1
-  # DELETE /car_requests/1.json
-  def destroy
-    @car_request.destroy
-    respond_to do |format|
-      format.html { redirect_to car_requests_url, notice: 'Car request was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_car_request
@@ -70,6 +46,6 @@ class CarRequestsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def car_request_params
-      params.require(:car_request).permit(:status, :scheduled_to, :driver_id_id, :passenger_id_id, :from_m, :to_m, :from_title, :to_title)
+      params.permit(:scheduled_to, :pickup_location, :drop_location, :pickup_address, :drop_address)
     end
 end
---
+++ app/controllers/car_searches_controller.rb
@@ -0,0 +1,63 @@
+class CarSearchesController < ApplicationController
+  before_action :set_car_search, only: [:show, :edit, :update, :destroy]
+
+  # GET /car_searches
+  # GET /car_searches.json
+  def index
+    @car_searches = CarSearch.all
+    p
+  end
+
+  # GET /car_searches/1
+  # GET /car_searches/1.json
+  def show
+    @routes = CarRouteSearcher.new.search(@car_search)
+  end
+
+  # GET /car_searches/new
+  def new
+    @car_search = CarSearch.new
+  end
+
+  # POST /car_searches
+  # POST /car_searches.json
+  def create
+    @car_search = CarSearch.new(car_search_params)
+
+    respond_to do |format|
+      if @car_search.save
+        format.html { redirect_to @car_search }
+        format.json { render :show, status: :created, location: @car_search }
+      else
+        format.html { render :new }
+        format.json { render json: @car_search.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /car_searches/1
+  # PATCH/PUT /car_searches/1.json
+  def update
+    respond_to do |format|
+      if @car_search.update(car_search_params)
+        format.html { redirect_to @car_search, notice: 'Car search was successfully updated.' }
+        format.json { render :show, status: :ok, location: @car_search }
+      else
+        format.html { render :edit }
+        format.json { render json: @car_search.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_car_search
+      @car_search = CarSearch.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def car_search_params
+      params.require(:car_search).permit(:scheduled_to, :from_title, :to_title, :from_m, :to_m, :pinned)
+      .tap{|params| params[:user] = current_user}
+    end
+end
