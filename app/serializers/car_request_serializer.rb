a78c76a25e6ac00740901548921794699bf7fd1e
Serge Tyatin <700@2rba.com>
2015-07-22 22:42:31 +0300

    removes malformed file

--- app/serializers/car_request_serializer.rb
+++
@@ -1,5 +0,0 @@
-class CarRequestSerializer < ActiveModel::Serializer
-  attributes :id, :status, :scheduled_to, :from_m, :to_m, :from_title, :to_title
-end
-  attribute :driver_id
-  attribute :passenger_id
