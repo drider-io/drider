8e83038764339bc01e960cc27c141eef110424da
Serge Tyatin <700@2rba.com>
2015-07-26 22:30:26 +0300

    download without auth

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,5 +1,4 @@
 class DownloadsController < ApplicationController
-  before_action :user_required
 
   def android
     send_file 'lib/downloads/android/drider_v1.apk', :type => 'application/vnd.android.package-archive', :disposition => 'attachment'
