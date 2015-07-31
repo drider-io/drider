d7e2f04afffdd20da3282a65daaa3137f741e64c
Serge Tyatin <700@2rba.com>
2015-07-31 23:35:12 +0300

    Migration fix

--- db/migrate/20150731171945_add_optimistic_locking_to_car_sessions.rb
+++ db/migrate/20150731171945_add_optimistic_locking_to_car_sessions.rb
@@ -1,5 +1,5 @@
 class AddOptimisticLockingToCarSessions < ActiveRecord::Migration
   def change
-    add_column :car_sessions, :lock_version, :integer
+    add_column :car_sessions, :lock_version, :integer, default: 0, null:false
   end
 end
