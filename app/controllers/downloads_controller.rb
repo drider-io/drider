d14692ea0278419cbff28db9a00bb4ec04917714
Serge Tyatin <700@2rba.com>
2015-07-31 22:47:59 +0300

    Adnroid version update

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,6 +1,6 @@
 class DownloadsController < ApplicationController
 
   def android
-    send_file 'lib/downloads/android/drider_v1.1.apk', :type => 'application/vnd.android.package-archive', :disposition => 'attachment'
+    send_file 'lib/downloads/android/drider_v1.2.apk', :type => 'application/vnd.android.package-archive', :disposition => 'attachment'
   end
 end
