1dac206085ceeb045720e292127cd3be5efab8b4
Serge Tyatin <700@2rba.com>
2015-08-02 10:52:12 +0300

    Fixes rendering bugs

--- app/assets/javascripts/landing/spirit8/main.js
+++ app/assets/javascripts/landing/spirit8/main.js
@@ -30,8 +30,8 @@ function main() {
     Show Menu on Book
     ======================================*/
     $(window).bind('scroll', function() {
-        var navHeight = $(window).height() - 100;
-        if ($(window).scrollTop() > navHeight) {
+        //var navHeight = $(window).height() - 100;
+        if ($(window).scrollTop() > 0) {
             $('.navbar-default').addClass('on');
         } else {
             $('.navbar-default').removeClass('on');
