2d629b40b159e645491cf40fc50147de633fc8db
Serge Tyatin <700@2rba.com>
2015-09-27 00:31:02 +0300

    Android client update to 1.4

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,6 +1,6 @@
 class DownloadsController < ApplicationController
 
   def android
-    send_file 'lib/downloads/android/drider_v1.3.apk', :type => 'application/vnd.android.package-archive', :disposition => 'attachment'
+    send_file 'lib/downloads/android/drider_v1.4.apk', :type => 'application/vnd.android.package-archive', :disposition => 'attachment'
   end
 end
