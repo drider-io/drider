08ad3bc84b6cf9f397727b3c5b8b5c2bf9d9d257
Serge Tyatin <700@2rba.com>
2016-04-15 23:54:47 +0300

    Configure ActiveJob to user sidekiq

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module Car
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
+    config.active_job.queue_adapter = :sidekiq
+
     Figaro.load
     config.mailgun = {
         :address => "smtp.mailgun.org",
