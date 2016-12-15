3863b80ea1d4b9fe8668b63a78e7ec15edf9121a
Serge Tyatin <700@2rba.com>
2016-12-15 16:31:00 +0200

    Disable subscribers

--- config/initializers/subscribers.rb
+++ config/initializers/subscribers.rb
@@ -1,6 +1,6 @@
-ActiveSupport::Notifications.subscribe('car_request_changed') do |name, start, ending, transaction_id, payload|
-  MessageGenerator.new.on_car_request_changed(payload[:request])
-end
-ActiveSupport::Notifications.subscribe('message_saved') do |name, start, ending, transaction_id, payload|
-  ExternalNotificationGenerator.new.on_message_saved(payload[:message])
-end
+# ActiveSupport::Notifications.subscribe('car_request_changed') do |name, start, ending, transaction_id, payload|
+#   MessageGenerator.new.on_car_request_changed(payload[:request])
+# end
+# ActiveSupport::Notifications.subscribe('message_saved') do |name, start, ending, transaction_id, payload|
+#   ExternalNotificationGenerator.new.on_message_saved(payload[:message])
+# end
