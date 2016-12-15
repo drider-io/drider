8fc0677a657aca00f4ae133790a3e6eb95e614b4
Serge Tyatin <700@2rba.com>
2016-12-15 16:30:09 +0200

    Add state machine to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,15 @@ class User < ActiveRecord::Base
   has_many :car_routes
   has_many :devices
   has_many :car_searches
+  belongs_to :last_search, class_name: 'CarSearch'
 
   before_save :ensure_authentication_token
   after_create :send_welcome_mail
 
-  def self.from_omniauth(auth, email)
+  scope :fb_chat_authed, -> { where('fb_chat_id IS NOT NULL') }
+
+  def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name   # assuming the user model has a name
       user.image_url = auth.info.image # assuming the user model has an image
@@ -62,6 +64,14 @@ class User < ActiveRecord::Base
     "https://graph.facebook.com/#{uid}/picture?type=square"
   end
 
+  def image_url191
+    "http://drider.io.rsz.io/profile_picture/#{uid}?width=382&height=200&bgcolor=fff"
+  end
+
+  def identified?
+    uid.present?
+  end
+
   private
 
   def generate_authentication_token
@@ -73,5 +83,58 @@ class User < ActiveRecord::Base
 
   def send_welcome_mail
       UserMailer.welcome(id).deliver_later
+  end
+
+
+  include AASM
+  aasm :column => 'bot_state' do
+    state :new, :initial => true
+    state :p_from
+    state :p_to
+    state :p_time
+    state :p_search
+    state :p_history
+
+    event :new_search, after: :notify_users do
+      after do |transition, message|
+        FbMessage.new(fb_chat_id).quick_replies(text: 'Звідки: вкажіть координату', replies: :location).deliver
+      end
+      transitions :to => :p_from
+    end
+
+    event :p_from_entered, after: :notify_users do
+      after do |transition, message|
+        FbMessage.new(fb_chat_id).quick_replies(text: 'Куди: вкажіть координату', replies: :location).deliver
+      end
+
+      transitions :from => :p_from, :to => :p_to
+    end
+
+    event :p_to_entered, after: :notify_users do
+      transitions :from => :p_to, :to => :p_time
+    end
+
+    event :go_now, after: :notify_users do
+      after do |transition, message|
+        CarRouteSearcher.new.drivers(last_search).limit(10).each do |driver|
+          CarRequest.create(
+            car_search: last_search,
+            driver: driver,
+            passenger: self,
+            status: :sent
+          # pickup_location: last_search.from_m,
+          # pickup_address: last_search.from_title,
+          # drop_location: last_search.to_m,
+          # drop_address: last_search.to_title
+          )
+          DriverNotifier.new(driver).perform
+        end
+      end
+      transitions :from => :p_time, :to => :p_search
+    end
+  end
+  
+  def notify_users
+    
   end
 end
