2595a9683d3abb2257b6239e6eeb333bf5aa7f45
Serge Tyatin <700@2rba.com>
2016-12-15 16:31:19 +0200

    Turn on fb subscribers

--- config/initializers/facebook_bot.rb
+++ config/initializers/facebook_bot.rb
@@ -5,14 +5,20 @@ Facebook::Messenger.configure do |config|
 end
 
 include Facebook::Messenger
-Bot.on :message do |message|
-  Rails.logger.debug message.inspect
-  Rails.logger.debug  "message.id #{message.id}"          # => 'mid.1457764197618:41d102a3e1ae206a38'
-  Rails.logger.debug  "message.sender #{message.sender}"      # => { 'id' => '1008372609250235' }
-  message.seq         # => 73
-  message.sent_at     # => 2016-04-22 21:30:36 +0200
-  message.text        # => 'Hello, bot!'
-  message.attachments # => [ { 'type' => 'image', 'payload' => { 'url' => 'https://www.example.com/1.jpg' } } ]
+Bot.on :message do |message| MessagesRouter.new(message).perform end
+Bot.on :postback do |message| PostbacksRouter.new(message).perform end
+# Bot.on :optin do |message| FbRouter.new(message).perform end
+# Bot.on :account_linking do |message| FbRouter.new(message).perform end
+
+  # Rails.logger.debug message.inspect
+  # Rails.logger.debug  "message.id #{message.id}"          # => 'mid.1457764197618:41d102a3e1ae206a38'
+  # Rails.logger.debug  "message.sender #{message.sender}"      # => { 'id' => '1008372609250235' }
+  # message.seq         # => 73
+  # message.sent_at     # => 2016-04-22 21:30:36 +0200
+  # message.text        # => 'Hello, bot!'
+  # message.attachments # => [ { 'type' => 'image', 'payload' => { 'url' => 'https://www.example.com/1.jpg' } } ]
+
+
 
   # Bot.deliver(
   #   recipient: message.sender,
@@ -20,4 +26,4 @@ Bot.on :message do |message|
   #     text: 'Hello, human!'
   #   }
   # )
-end
+
