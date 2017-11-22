5c4dc37d22b4234c1ddb45272d8e213e2d424df3
Serge Tyatin <700@2rba.com>
2017-11-22 13:29:33 +0200

    Add message notifier

---
+++ app/parts/message_notifier.rb
@@ -0,0 +1,26 @@
+class MessageNotifier
+  def initialize(user)
+    @user = user
+  end
+
+  def perform
+    message = FbMessage.new(@user.fb_chat_id)
+
+    corespondent_ids = Message.unread(@user).distinct.group('from_id').pluck(:from_id,'max(created_at)').sort_by(&:last).reverse
+
+    corespondent_ids.each do |ar|
+      origin = User.find(ar.first)
+      message.generic_template(title: "Повідомлення від #{origin.name}",
+                               subtitle: Message.where(from_id: origin.id, to_id: @user.id).order('created_at DESC').first.body,
+                               image_url: origin.image_url191,
+                               buttons: [
+                                         {
+                                           type: 'web_url',
+                                           title: 'Повідомлення',
+                                           url: Rails.application.routes.url_helpers.message_url(origin.id) + "?auth_token=#{origin.authentication_token}",
+                                         }
+                               ])
+    end
+    message.deliver
+  end
+end
